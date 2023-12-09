fd0db6eea4186453ce87fe726c02ce295a44868c
Harrel <marcin.slowik95@gmail.com>
2023-12-09 16:24:59 +0100

    add logger ot container

--- server/src/test/java/dev/harrel/jarhell/Neo4jTest.java
+++ server/src/test/java/dev/harrel/jarhell/Neo4jTest.java
@@ -2,8 +2,10 @@ package dev.harrel.jarhell;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.neo4j.driver.*;
+import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.Neo4jContainer;
 import org.testcontainers.containers.Neo4jLabsPlugin;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
@@ -12,6 +14,7 @@ import org.testcontainers.utility.DockerImageName;
 public abstract class Neo4jTest {
     @Container
     private static final Neo4jContainer<?> neo4jContainer = new Neo4jContainer<>(DockerImageName.parse("neo4j:5.11"))
+            .withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger(Neo4jTest.class)))
             .withLabsPlugins(Neo4jLabsPlugin.APOC)
             .withoutAuthentication();
 
