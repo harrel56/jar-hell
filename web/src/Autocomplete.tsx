3b30bae8f2a2fbb8aa12e1a006168fd516b50b5b
Harrel <marcin.slowik95@gmail.com>
2024-01-11 18:29:10 +0100

    stylings

--- web/src/Autocomplete.tsx
+++ web/src/Autocomplete.tsx
@@ -1,6 +1,5 @@
-import {useAutocomplete} from '@mui/base'
+import {useAutocomplete, UseAutocompleteReturnValue} from '@mui/base/useAutocomplete/useAutocomplete'
 import {useEffect, useState} from 'react'
-import {UseAutocompleteReturnValue} from '@mui/base/useAutocomplete/useAutocomplete'
 import {useDebounce} from 'use-debounce'
 import {useApiFetch} from './hooks/useApiFetch.ts'
 import {useNavigate} from 'react-router-dom'
@@ -17,7 +16,14 @@ interface ListboxProps {
   ac: UseAutocompleteReturnValue<Artifact>
 }
 
-const artifactString = (artifact: Artifact) => `${artifact.g}:${artifact.a}`
+const toArtifactString = (artifact: Artifact) => `${artifact.g}:${artifact.a}`
+const toShortArtifactString = (artifact: Artifact) => {
+  const idx = artifact.a.indexOf(artifact.g)
+  if (idx === 0) {
+    return toArtifactString({...artifact, a: `[...]${artifact.a.slice(artifact.g.length)}`})
+  }
+  return toArtifactString(artifact)
+}
 
 const Listbox = ({loading, debouncing, ac}: ListboxProps) => {
   if (!ac.popupOpen) {
@@ -25,22 +31,23 @@ const Listbox = ({loading, debouncing, ac}: ListboxProps) => {
   }
   const noResultsFound = !loading && !debouncing && ac.inputValue !== '' && ac.groupedOptions.length === 0
   return (
-    <ul {...ac.getListboxProps()}>
-      {loading && <div>Loading...</div>}
-      {noResultsFound &&
-        <div>No results found</div>
-      }
-      {(ac.groupedOptions as Artifact[]).map((option, index) => (
-        <li {...ac.getOptionProps({option, index})}>
-          {artifactString(option)}
-        </li>
-      ))}
-    </ul>
+    <div className='relative cursor-pointer'>
+      <ul className='absolute flex flex-col overflow-y-auto max-h-96 gap-0.5 border-2 border-emerald-700' {...ac.getListboxProps()}>
+        {loading && <li>Loading...</li>}
+        {noResultsFound &&
+          <li>No results found</li>
+        }
+        {(ac.groupedOptions as Artifact[]).map((option, index) => (
+          <li className='p-2 bg-amber-100 whitespace-nowrap' {...ac.getOptionProps({option, index})}>
+            {toShortArtifactString(option)}
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
 export const Autocomplete = () => {
-  const [value] = useState<Artifact | null>(null)
   const [inputValue, setInputValue] = useState('')
   const [debouncedInput, {isPending}] = useDebounce(inputValue, 500)
   const [options, setOptions] = useState<Artifact[]>([])
@@ -48,7 +55,7 @@ export const Autocomplete = () => {
 
   const onPackageSelect = (val: Artifact | null) => {
     if (val !== null) {
-      navigate(`/packages/${artifactString(val)}:${val.latestVersion}`)
+      navigate(`/packages/${toArtifactString(val)}:${val.latestVersion}`)
     }
   }
   const {
@@ -75,18 +82,16 @@ export const Autocomplete = () => {
     id: 'packages-autocomplete',
     options: options,
     filterOptions: options => options,
-    getOptionLabel: artifactString,
-    isOptionEqualToValue: (a1, a2) => artifactString(a1) === artifactString(a2),
-    value,
+    getOptionLabel: toArtifactString,
+    isOptionEqualToValue: (a1, a2) => toArtifactString(a1) === toArtifactString(a2),
     onChange: (_event, newValue) => onPackageSelect(newValue),
     inputValue,
     onInputChange: (_event, newInputValue) => setInputValue(newInputValue)
   })
 
   return (
-    <div className='w-96 flex flex-col gap-1' {...ac.getRootProps()}>
-      {value && <p>{artifactString(value)}</p>}
-      <label {...ac.getInputLabelProps()}>Hello</label>
+    <div className='w-96 flex flex-col font-mono' {...ac.getRootProps()}>
+      {/*<label {...ac.getInputLabelProps()}>Hello</label>*/}
       <input className='h-10 p-1' {...ac.getInputProps()} value={inputValue}/>
       <Listbox loading={loading} debouncing={isPending()} ac={ac}/>
     </div>
