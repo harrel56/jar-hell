21c9dd12359af9f3855d5f8f6e143b9b92513c11
Harrel <marcin.slowik95@gmail.com>
2024-10-05 23:09:28 +0200

    let's try out shadcn

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,12 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        }
+    }
 })
