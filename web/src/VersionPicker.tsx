e9900ebc2c8918241c43d900c1151e1bd77d45c7
Harrel <marcin.slowik95@gmail.com>
2024-10-09 20:40:23 +0200

    add rather tedious version grouping

--- web/src/VersionPicker.tsx
+++ web/src/VersionPicker.tsx
@@ -7,16 +7,7 @@ export interface VersionPickerProps {
 }
 
 export const VersionPicker = ({versions}: VersionPickerProps) => {
-  const versionNodes: Map<string, string[]> = useMemo(() => {
-    const byMajor = new Map<string, string[]>()
-    versions.forEach(version => {
-      const [major] = version.split('.', 1)
-      const newVersions = byMajor.get(major) ?? []
-      newVersions.push(version);
-      byMajor.set(major, newVersions)
-    })
-    return byMajor
-  }, [versions])
+  const versionNodes = useMemo(() => calculateVersionNodes(versions), [versions])
 
   return (
     <Accordion type="single" collapsible className="w-full">
@@ -44,4 +35,37 @@ export const VersionPicker = ({versions}: VersionPickerProps) => {
       ))}
     </Accordion>
   )
+}
+
+/**
+ * Group by minor version if:
+ * - there is only 1 major
+ * - in scope of 1 major there is a minor that got >= 10 patch versions
+ * otherwise group by major
+ * */
+const calculateVersionNodes = (versions: string[]) => {
+  const byMajor = new Map<string, Map<string, string[]>>()
+  versions.forEach(version => {
+    const [major, minor] = version.split('.', 2)
+    const byMinor = byMajor.get(major) ?? new Map<string, string[]>()
+    const patches = byMinor.get(minor) ?? []
+    patches.push(version)
+    byMinor.set(minor, patches)
+    byMajor.set(major, byMinor)
+  })
+
+  const nodes = new Map<string, string[]>()
+  Array.from(byMajor.entries()).forEach(([major, byMinor]) => {
+    const expandMinor = byMajor.size === 1 || Array.from(byMinor.values()).some(patches => patches.length >= 10)
+    if (expandMinor) {
+      Array.from(byMinor.entries()).forEach(([minor, patches]) => {
+        nodes.set(`${major}.${minor}`, patches)
+      })
+    } else {
+      const newVersions: string[] = []
+      Array.from(byMinor.values()).forEach(patches => newVersions.push(...patches))
+      nodes.set(major, newVersions)
+    }
+  })
+  return nodes
 }
