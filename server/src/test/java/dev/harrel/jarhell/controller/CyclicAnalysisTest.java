ed495f1ba824c97c6b2fb43c4cd11d3ec210c442
Harrel <marcin.slowik95@gmail.com>
2024-10-25 18:14:21 +0200

    fix tests

--- server/src/test/java/dev/harrel/jarhell/controller/CyclicAnalysisTest.java
+++ server/src/test/java/dev/harrel/jarhell/controller/CyclicAnalysisTest.java
@@ -1,12 +1,10 @@
 package dev.harrel.jarhell.controller;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import dev.harrel.jarhell.error.ErrorResponse;
 import dev.harrel.jarhell.extension.EnvironmentTest;
 import dev.harrel.jarhell.extension.Host;
 import dev.harrel.jarhell.model.Gav;
 import dev.harrel.jarhell.util.HttpUtil;
-import io.javalin.http.HandlerType;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -20,6 +18,7 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @EnvironmentTest
+@SuppressWarnings("unchecked")
 class CyclicAnalysisTest {
     private final HttpClient httpClient;
 
@@ -31,7 +30,7 @@ class CyclicAnalysisTest {
     }
 
     @Test
-    void hardCycleWithSelfFails() throws IOException, InterruptedException {
+    void hardCycleWithSelfIsIgnored() throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(host + "/api/v1/analyze-and-wait"))
                 .POST(HttpUtil.jsonPublisher(
@@ -39,18 +38,19 @@ class CyclicAnalysisTest {
                 ))
                 .build();
 
-        HttpResponse<ErrorResponse> response = httpClient.send(request, HttpUtil.jsonHandler(ErrorResponse.class));
-        assertThat(response.statusCode()).isEqualTo(500);
-        assertThat(response.body()).isEqualTo(
-                new ErrorResponse(host + "/api/v1/analyze-and-wait",
-                        HandlerType.POST,
-                        "HARD CYCLE found, preceding: [], cycle: [org.test:cycle-self:1.0.0, org.test:cycle-self:1.0.0]")
-        );
+        HttpResponse<Map<String, Object>> response = httpClient.send(request, HttpUtil.jsonHandler(new TypeReference<>() {}));
+        assertThat(response.statusCode()).isEqualTo(200);
+        Map<String, Object> properties = response.body();
+        assertThat(properties)
+                .containsEntry("artifactId", "cycle-self")
+                .containsEntry("packageSize", 2105L)
+                .containsEntry("totalSize", 2105L);
+        List<Map<String, Object>> deps = (List<Map<String, Object>>) properties.get("dependencies");
+        assertThat(deps).isEmpty();
     }
 
     @Test
-    @SuppressWarnings("unchecked")
-    void softCycleWithSelfPasses() throws IOException, InterruptedException {
+    void softCycleWithSelfIsIgnored() throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(host + "/api/v1/analyze-and-wait"))
                 .POST(HttpUtil.jsonPublisher(
@@ -61,20 +61,15 @@ class CyclicAnalysisTest {
         HttpResponse<Map<String, Object>> response = httpClient.send(request, HttpUtil.jsonHandler(new TypeReference<>() {}));
         assertThat(response.statusCode()).isEqualTo(200);
         Map<String, Object> properties = response.body();
-        assertThat(properties).containsEntry("artifactId", "cycle-self-soft");
+        assertThat(properties).containsEntry("artifactId", "cycle-self-soft")
+                .containsEntry("packageSize", 2105L)
+                .containsEntry("totalSize", 2105L);
         List<Map<String, Object>> deps = (List<Map<String, Object>>) properties.get("dependencies");
-        assertThat(deps).hasSize(1);
-        assertThat(deps.getFirst())
-                .containsEntry("optional", true)
-                .containsEntry("scope", "compile");
-        Map<String, Object> depProps = (Map<String, Object>) deps.getFirst().get("artifact");
-        assertThat(depProps)
-                .containsEntry("artifactId", "cycle-self-soft")
-                .doesNotContainKey("dependencies");
+        assertThat(deps).isEmpty();
     }
 
     @Test
-    void hardCycleWithThreeArtifactsFails() throws IOException, InterruptedException {
+    void hardCycleWithThreeArtifactsIsComputedCorrectly() throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(host + "/api/v1/analyze-and-wait"))
                 .POST(HttpUtil.jsonPublisher(
@@ -82,17 +77,26 @@ class CyclicAnalysisTest {
                 ))
                 .build();
 
-        HttpResponse<ErrorResponse> response = httpClient.send(request, HttpUtil.jsonHandler(ErrorResponse.class));
-        assertThat(response.statusCode()).isEqualTo(500);
-        assertThat(response.body()).isEqualTo(
-                new ErrorResponse(host + "/api/v1/analyze-and-wait",
-                        HandlerType.POST,
-                        "HARD CYCLE found, preceding: [], cycle: [org.test:cycle1:1.0.0, org.test:cycle2:1.0.0, org.test:cycle3:1.0.0, org.test:cycle1:1.0.0]")
-        );
+        HttpResponse<Map<String, Object>> response = httpClient.send(request, HttpUtil.jsonHandler(new TypeReference<>() {}));
+        assertThat(response.statusCode()).isEqualTo(200);
+        Map<String, Object> properties = response.body();
+        assertThat(properties)
+                .containsEntry("artifactId", "cycle1")
+                .containsEntry("packageSize", 2105L)
+                .containsEntry("totalSize", 6315L);
+        List<Map<String, Object>> deps = (List<Map<String, Object>>) properties.get("dependencies");
+        assertThat(deps).hasSize(1);
+        assertThat(deps.getFirst())
+                .containsEntry("optional", false)
+                .containsEntry("scope", "compile");
+        assertThat((Map<String, Object>) deps.getFirst().get("artifact"))
+                .containsEntry("artifactId", "cycle2")
+                .containsEntry("packageSize", 2105L)
+                .containsEntry("totalSize", 6315L);
     }
 
     @Test
-    void hardCycleWithFourArtifactsFails() throws IOException, InterruptedException {
+    void hardCycleWithFourArtifactsIsComputedCorrectly() throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(host + "/api/v1/analyze-and-wait"))
                 .POST(HttpUtil.jsonPublisher(
@@ -100,38 +104,21 @@ class CyclicAnalysisTest {
                 ))
                 .build();
 
-        HttpResponse<ErrorResponse> response = httpClient.send(request, HttpUtil.jsonHandler(ErrorResponse.class));
-        assertThat(response.statusCode()).isEqualTo(500);
-        assertThat(response.body()).isEqualTo(
-                new ErrorResponse(host + "/api/v1/analyze-and-wait",
-                        HandlerType.POST,
-                        "HARD CYCLE found, preceding: [org.test:pre-cycle:1.0.0], cycle: [org.test:cycle3:1.0.0, org.test:cycle1:1.0.0, org.test:cycle2:1.0.0, org.test:cycle3:1.0.0]")
-        );
+        HttpResponse<Map<String, Object>> response = httpClient.send(request, HttpUtil.jsonHandler(new TypeReference<>() {}));
+        assertThat(response.statusCode()).isEqualTo(200);
+        Map<String, Object> properties = response.body();
+        assertThat(properties)
+                .containsEntry("artifactId", "pre-cycle")
+                .containsEntry("packageSize", 2105L)
+                .containsEntry("totalSize", 2105L); // all deps are optional
+        List<Map<String, Object>> deps = (List<Map<String, Object>>) properties.get("dependencies");
+        assertThat(deps).hasSize(1);
+        assertThat(deps.getFirst())
+                .containsEntry("optional", true)
+                .containsEntry("scope", "compile");
+        assertThat((Map<String, Object>) deps.getFirst().get("artifact"))
+                .containsEntry("artifactId", "cycle3")
+                .containsEntry("packageSize", 2105L)
+                .containsEntry("totalSize", 6315L);
     }
-
-//    @Test
-//    @SuppressWarnings("unchecked")
-//    void softCycleWithThreeArtifactsPasses() throws IOException, InterruptedException {
-//        HttpRequest request = HttpRequest.newBuilder()
-//                .uri(URI.create(host + "/api/v1/analyze-and-wait"))
-//                .POST(HttpUtil.jsonPublisher(
-//                        new Gav("org.test", "cycle1-soft", "1.0.0")
-//                ))
-//                .build();
-//
-//        HttpResponse<Map<String, Object>> response = httpClient.send(request, HttpUtil.jsonHandler(new TypeReference<>() {}));
-//        assertThat(response.statusCode()).isEqualTo(200);
-//        Map<String, Object> properties = response.body();
-//        assertThat(properties).containsEntry("artifactId", "cycle1-soft");
-//        List<Map<String, Object>> deps = (List<Map<String, Object>>) properties.get("dependencies");
-//        assertThat(deps).hasSize(2);
-//        assertThat(deps.get(0))
-//                .containsEntry("optional", true)
-//                .containsEntry("scope", "compile");
-//        assertThat(deps.get(1))
-//                .containsEntry("optional", true)
-//                .containsEntry("scope", "compile");
-//        Map<String, Object> depProps1 = (Map<String, Object>) deps.get(0).get("artifact");
-//        Map<String, Object> depProps2 = (Map<String, Object>) deps.get(1).get("artifact");
-//    }
 }
