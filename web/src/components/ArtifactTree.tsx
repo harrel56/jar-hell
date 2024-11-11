8ba81d9e7d1b1235fb0f4d619ca29021e2c21ed1
Harrel <marcin.slowik95@gmail.com>
2024-11-11 20:44:17 +0100

    ArtifactTree cleaning up

--- web/src/components/ArtifactTree.tsx
+++ web/src/components/ArtifactTree.tsx
@@ -1,8 +1,8 @@
 import {gavToString, getAllDepsCount, isResolvedPackage, Package} from '@/util.ts'
 import * as AccordionPrimitive from '@radix-ui/react-accordion'
-import {ChevronDown, CircleChevronDown} from 'lucide-react'
+import {Circle, CircleChevronDown} from 'lucide-react'
 import {useFetch} from '@/hooks/useFetch.ts'
-import {useState} from 'react'
+import {forwardRef, useState} from 'react'
 import {LoadingSpinner} from '@/components/LoadingSpinner.tsx'
 
 export interface PackageTreeProperties {
@@ -32,7 +32,11 @@ const DepNode = ({node}: { node: Package }) => {
     }
   }
   if (!isResolvedPackage(artifact) || getAllDepsCount(artifact) === 0) {
-    return <div className='text-faded py-3'>{gav}</div>
+    return (
+      <div className='text-faded py-3'>
+        <DepHeader node={artifact} loading={loading} leaf={true}/>
+      </div>
+    )
   }
 
   return (
@@ -41,18 +45,16 @@ const DepNode = ({node}: { node: Package }) => {
       <AccordionPrimitive.Item value={gav}>
         <AccordionPrimitive.Header className='flex items-center py-3'>
           <AccordionPrimitive.Trigger asChild className='flex transition-all [&[data-state=open]>svg]:rotate-180'>
-            <div className='flex items-center gap-2 cursor-pointer'>
-              {loading ? <LoadingSpinner/> :
-                <CircleChevronDown className='h-4 w-4 shrink-0 transition-transform duration-200' />}
-              <span>{gav}</span>
-            </div>
+            <DepHeader node={artifact} loading={loading}/>
           </AccordionPrimitive.Trigger>
         </AccordionPrimitive.Header>
         <AccordionPrimitive.Content asChild
                                     className='overflow-hidden text-sm ml-8 transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down'>
           <ul>
             {isResolvedPackage(artifact) && artifact.dependencies?.map(dep => (
-              <li><DepNode key={gavToString(dep.artifact)} node={dep.artifact}/></li>
+              <li key={gavToString(dep.artifact)}>
+                <DepNode node={dep.artifact}/>
+              </li>
             ))}
           </ul>
         </AccordionPrimitive.Content>
@@ -60,3 +62,28 @@ const DepNode = ({node}: { node: Package }) => {
     </AccordionPrimitive.Root>
   )
 }
+
+interface DepHeaderProps {
+  node: Package
+  loading: boolean
+  leaf?: boolean
+}
+
+const DepHeader = forwardRef<HTMLDivElement, DepHeaderProps>((props, ref) => {
+  const iconClasses = 'h-4 w-4 shrink-0 transition-transform duration-200'
+  const getIcon = () => {
+    if (props.loading) {
+      return <LoadingSpinner className={iconClasses}/>
+    }
+    if (props.leaf) {
+      return <Circle className={iconClasses}/>
+    }
+    return <CircleChevronDown className={iconClasses}/>
+  }
+  return (
+    <div ref={ref} {...props} className='flex items-center gap-2 cursor-pointer'>
+      {getIcon()}
+      <span>{gavToString(props.node)}</span>
+    </div>
+  )
+})
