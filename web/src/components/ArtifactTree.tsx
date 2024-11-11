7bb278ef80092867bbda64cfb46ddebb2a15022b
Harrel <marcin.slowik95@gmail.com>
2024-11-11 19:38:48 +0100

    some working styled tree

--- web/src/components/ArtifactTree.tsx
+++ web/src/components/ArtifactTree.tsx
@@ -1,6 +1,6 @@
 import {gavToString, getAllDepsCount, isResolvedPackage, Package} from '@/util.ts'
 import * as AccordionPrimitive from '@radix-ui/react-accordion'
-import {ChevronDown} from 'lucide-react'
+import {ChevronDown, CircleChevronDown} from 'lucide-react'
 import {useFetch} from '@/hooks/useFetch.ts'
 import {useState} from 'react'
 import {LoadingSpinner} from '@/components/LoadingSpinner.tsx'
@@ -11,7 +11,7 @@ export interface PackageTreeProperties {
 
 export const ArtifactTree = ({artifact}: PackageTreeProperties) => {
   return (
-      <DepNode node={artifact}/>
+    <DepNode node={artifact}/>
   )
 }
 
@@ -22,7 +22,6 @@ const DepNode = ({node}: { node: Package }) => {
   const artifact = data ?? node
 
   const onValueChange = (opened: string[]) => {
-    console.log(opened)
     if (loading) {
       return
     }
@@ -33,25 +32,29 @@ const DepNode = ({node}: { node: Package }) => {
     }
   }
   if (!isResolvedPackage(artifact) || getAllDepsCount(artifact) === 0) {
-    return <div>{gav}</div>
+    return <div className='text-faded py-3'>{gav}</div>
   }
 
   return (
-    <AccordionPrimitive.Root type='multiple' value={openedNodes} onValueChange={onValueChange}>
+    <AccordionPrimitive.Root type='multiple' value={openedNodes} onValueChange={onValueChange}
+                             className='tree text-left'>
       <AccordionPrimitive.Item value={gav}>
-        <AccordionPrimitive.Header className='flex'>
-          <AccordionPrimitive.Trigger
-            className='flex flex-1 items-center justify-between py-4 font-medium transition-all [&[data-state=open]>svg]:rotate-180'>
-            {loading ? <LoadingSpinner/> :
-              <ChevronDown className='h-4 w-4 shrink-0 transition-transform duration-200'/>}
-            <span>{gav}</span>
+        <AccordionPrimitive.Header className='flex items-center py-3'>
+          <AccordionPrimitive.Trigger asChild className='flex transition-all [&[data-state=open]>svg]:rotate-180'>
+            <div className='flex items-center gap-2 cursor-pointer'>
+              {loading ? <LoadingSpinner/> :
+                <CircleChevronDown className='h-4 w-4 shrink-0 transition-transform duration-200' />}
+              <span>{gav}</span>
+            </div>
           </AccordionPrimitive.Trigger>
         </AccordionPrimitive.Header>
-        <AccordionPrimitive.Content
-          className='overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down'>
-          {isResolvedPackage(artifact) && artifact.dependencies?.map(dep => (
-            <DepNode key={gavToString(dep.artifact)} node={dep.artifact}/>
-          ))}
+        <AccordionPrimitive.Content asChild
+                                    className='overflow-hidden text-sm ml-8 transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down'>
+          <ul>
+            {isResolvedPackage(artifact) && artifact.dependencies?.map(dep => (
+              <li><DepNode key={gavToString(dep.artifact)} node={dep.artifact}/></li>
+            ))}
+          </ul>
         </AccordionPrimitive.Content>
       </AccordionPrimitive.Item>
     </AccordionPrimitive.Root>
