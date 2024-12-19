dfcd83bb311ff209bf6d494f2b55072b0585cb41
Harrel <marcin.slowik95@gmail.com>
2024-12-19 22:27:28 +0100

    let's fallback to CFs

--- server/src/main/java/dev/harrel/jarhell/analyze/RepoWalker.java
+++ server/src/main/java/dev/harrel/jarhell/analyze/RepoWalker.java
@@ -1,6 +1,5 @@
 package dev.harrel.jarhell.analyze;
 
-import dev.harrel.jarhell.util.ConcurrentUtil;
 import org.apache.maven.artifact.versioning.ComparableVersion;
 import org.eclipse.jetty.client.HttpClient;
 import org.eclipse.jetty.client.api.ContentResponse;
@@ -11,11 +10,13 @@ import javax.inject.Singleton;
 import java.net.URI;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -26,71 +27,61 @@ public class RepoWalker {
     private final String repoUrl = "https://repo1.maven.org/maven2";//Config.get("maven.repo-url");
     private final HttpClient httpClient;
     private final AtomicLong counter = new AtomicLong();
-    private final Map<Integer, Semaphore> semaphores = Map.of(
-            0, new Semaphore(1),
-            1, new Semaphore(4),
-            2, new Semaphore(16),
-            3, new Semaphore(64),
-            4, new Semaphore(128)
-    );
+    private final ExecutorService executorService = Executors.newFixedThreadPool(256 * Runtime.getRuntime().availableProcessors(), Thread.ofVirtual().factory());
 
     public RepoWalker(HttpClient httpClient) {
         this.httpClient = httpClient;
     }
 
     public void walk(Consumer<State> consumer) {
-        walkInternal(consumer, List.of());
+        walkInternal(consumer, List.of()).join();
     }
 
-    private void walkInternal(Consumer<State> consumer, List<String> pathSegments) {
-        Semaphore semaphore = semaphores.getOrDefault(pathSegments.size(), new Semaphore(1));
-        if (semaphore.availablePermits() <= 0) {
-            System.out.println("WAIT: " + pathSegments.size());
-        }
-        semaphore.acquireUninterruptibly();
+    private CompletableFuture<?> walkInternal(Consumer<State> consumer, List<String> pathSegments) {
+        URI uri = segmentsToUri(pathSegments);
+        System.out.println(counter.incrementAndGet());
+        ContentResponse res;
         try {
-            URI uri = segmentsToUri(pathSegments);
-//            System.out.println(counter.incrementAndGet());
-            ContentResponse res;
-            try {
-                res = httpClient.GET(uri);
-            } catch (ExecutionException | TimeoutException e) {
-                throw new CompletionException(e);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                throw new CompletionException(e);
-            }
-            if (res.getStatus() >= 400) {
-                throw new IllegalArgumentException("HTTP call failed [%s] for url [%s]".formatted(res.getStatus(), uri));
-            }
-
-            Document doc = Jsoup.parse(res.getContentAsString());
-            List<String> dirs = doc.getElementsByTag("a").stream()
-                    .map(el -> el.attr("href"))
-                    .filter(href -> href.endsWith("/") && !href.equals("../"))
-                    .toList();
-            Map<Boolean, List<String>> partitioned = dirs.stream()
-                    .collect(Collectors.partitioningBy(dir -> HTML_VERSIONS_PATTERN.matcher(dir).matches()));
-            List<String> paths = partitioned.get(false);
-            List<String> versions = partitioned.get(true).stream()
-                    .map(v -> v.substring(0, v.indexOf('/')))
-                    .map(ComparableVersion::new)
-                    .sorted()
-                    .map(ComparableVersion::toString)
-                    .toList();
-
-            try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
-                if (!versions.isEmpty() && pathSegments.size() >= 2) {
-                    scope.fork(Executors.callable(() -> consumer.accept(createState(pathSegments, versions))));
-                }
-                for (String path : paths) {
-                    scope.fork(Executors.callable(() -> walkInternal(consumer, concatList(pathSegments, path))));
-                }
-                ConcurrentUtil.joinScope(scope);
-            }
-        } finally {
-            semaphore.release();
+            res = httpClient.GET(uri);
+        } catch (ExecutionException | TimeoutException e) {
+            throw new CompletionException(e);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new CompletionException(e);
         }
+        if (res.getStatus() >= 400) {
+            throw new IllegalArgumentException("HTTP call failed [%s] for url [%s]".formatted(res.getStatus(), uri));
+        }
+
+        Document doc = Jsoup.parse(res.getContentAsString());
+        List<String> dirs = doc.getElementsByTag("a").stream()
+                .map(el -> el.attr("href"))
+                .filter(href -> href.endsWith("/") && !href.equals("../"))
+                .toList();
+        Map<Boolean, List<String>> partitioned = dirs.stream()
+                .collect(Collectors.partitioningBy(dir -> HTML_VERSIONS_PATTERN.matcher(dir).matches()));
+        List<String> paths = partitioned.get(false);
+        List<String> versions = partitioned.get(true).stream()
+                .map(v -> v.substring(0, v.indexOf('/')))
+                .map(ComparableVersion::new)
+                .sorted()
+                .map(ComparableVersion::toString)
+                .toList();
+
+        List<CompletableFuture<?>> futures = new ArrayList<>(paths.size() + 1);
+        if (!versions.isEmpty() && pathSegments.size() >= 2) {
+            futures.add(CompletableFuture.supplyAsync(() -> {
+                consumer.accept(createState(pathSegments, versions));
+                return null;
+            }, executorService));
+        }
+        for (String path : paths) {
+            CompletableFuture<?> cf = CompletableFuture.supplyAsync(
+                            () -> walkInternal(consumer, concatList(pathSegments, path)), executorService)
+                    .thenCompose(Function.identity());
+            futures.add(cf);
+        }
+        return CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
     }
 
     private URI segmentsToUri(List<String> pathSegments) {
