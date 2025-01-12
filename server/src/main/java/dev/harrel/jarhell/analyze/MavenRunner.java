0e7368a3966aa9225f51d044bd0454d5294b5742
Harrel <marcin.slowik95@gmail.com>
2025-01-12 12:52:50 +0100

    handle dep collections exceptions

--- server/src/main/java/dev/harrel/jarhell/analyze/MavenRunner.java
+++ server/src/main/java/dev/harrel/jarhell/analyze/MavenRunner.java
@@ -20,19 +20,27 @@ import org.eclipse.aether.collection.CollectResult;
 import org.eclipse.aether.collection.DependencyCollectionException;
 import org.eclipse.aether.graph.Dependency;
 import org.eclipse.aether.graph.DependencyNode;
+import org.eclipse.aether.metadata.Metadata;
 import org.eclipse.aether.repository.RemoteRepository;
 import org.eclipse.aether.resolution.ArtifactDescriptorException;
 import org.eclipse.aether.resolution.ArtifactDescriptorRequest;
 import org.eclipse.aether.resolution.ArtifactDescriptorResult;
+import org.eclipse.aether.resolution.ArtifactResolutionException;
+import org.eclipse.aether.transfer.ArtifactTransferException;
+import org.eclipse.aether.transfer.MetadataTransferException;
 import org.eclipse.aether.util.graph.visitor.PreorderNodeListGenerator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Singleton;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 @Singleton
 class MavenRunner {
     private static final String MAVEN_CENTRAL = Config.get("maven.repo-url");
+    private static final Logger logger = LoggerFactory.getLogger(MavenRunner.class);
 
     private final RepositorySystem repoSystem;
     private final RepositorySystemSession session;
@@ -46,27 +54,28 @@ class MavenRunner {
 
     public CollectedDependencies collectDependencies(Gav gav) {
         CollectRequest request = createCollectRequest(gav);
+        CollectResult collectResult;
         try {
-            CollectResult collectResult = repoSystem.collectDependencies(session, request);
-            if (!collectResult.getExceptions().isEmpty()) {
-                throw new IllegalArgumentException("Errors found in %s: %s".formatted(gav, collectResult.getExceptions()));
-            }
-
-            List<FlatDependency> directDependencies = collectResult.getRoot().getChildren().stream()
-                    .map(DependencyNode::getDependency)
-                    .map(MavenRunner::toFlatDependency)
-                    .toList();
-            PreorderNodeListGenerator visitor = new PreorderNodeListGenerator();
-            collectResult.getRoot().accept(visitor);
-            List<FlatDependency> allDependencies = visitor.getDependencies(true).stream()
-                    .map(MavenRunner::toFlatDependency)
-                    .filter(dep -> !dep.gav().equals(gav))
-                    .toList();
-
-            return new CollectedDependencies(directDependencies, allDependencies);
+            collectResult = repoSystem.collectDependencies(session, request);
         } catch (DependencyCollectionException e) {
-            throw new IllegalArgumentException(e);
+            logger.warn("Dependency collection failed", e);
+            collectResult = e.getResult();
         }
+
+        List<FlatDependency> directDependencies = collectResult.getRoot().getChildren().stream()
+                .map(DependencyNode::getDependency)
+                .map(MavenRunner::toFlatDependency)
+                .toList();
+
+        PreorderNodeListGenerator visitor = new PreorderNodeListGenerator();
+        collectResult.getRoot().accept(visitor);
+        Stream<FlatDependency> allDependenciesStream = visitor.getDependencies(true).stream()
+                .map(MavenRunner::toFlatDependency)
+                .filter(dep -> !dep.gav().equals(gav));
+        Stream<FlatDependency> failedDepsStream = getFailureCauses(collectResult.getExceptions());
+        List<FlatDependency> allDependencies = Stream.concat(allDependenciesStream, failedDepsStream).toList();
+
+        return new CollectedDependencies(directDependencies, allDependencies);
     }
 
     // todo: this should just use ModelBuilder, then we can remove CustomDescriptorReaderDelegate
@@ -108,13 +117,44 @@ class MavenRunner {
         return collectRequest;
     }
 
-    private static Gav toGav(Dependency dep) {
-        Artifact artifact = dep.getArtifact();
+    private static Gav toGav(Artifact artifact) {
         return new Gav(artifact.getGroupId(), artifact.getArtifactId(), artifact.getVersion(), artifact.getClassifier());
+    }
+
+    private static Gav toGav(Dependency dep) {
+        return toGav(dep.getArtifact());
     }
 
     private static FlatDependency toFlatDependency(Dependency dep) {
         String scope = dep.getScope().isBlank() ? "compile" : dep.getScope();
         return new FlatDependency(toGav(dep), dep.isOptional(), scope);
+    }
+
+    private static FlatDependency toFlatDependency(Artifact artifact) {
+        return new FlatDependency(toGav(artifact), false, "compile");
+    }
+
+    private static FlatDependency toFlatDependency(Metadata metadata) {
+        Gav gav = new Gav(metadata.getGroupId(), metadata.getArtifactId(), metadata.getVersion());
+        return new FlatDependency(gav, false, "compile");
+    }
+
+    private static Stream<FlatDependency> getFailureCauses(List<Exception> exceptions) {
+        return exceptions.stream()
+                .<List<FlatDependency>>map(e ->
+                        switch (e) {
+                            case ArtifactDescriptorException ade ->
+                                    List.of(toFlatDependency(ade.getResult().getArtifact()));
+                            case ArtifactResolutionException are ->
+                                    are.getResults().stream()
+                                    .map(a -> toFlatDependency(a.getArtifact()))
+                                    .toList();
+                            case ArtifactTransferException ate ->
+                                List.of(toFlatDependency(ate.getArtifact()));
+                            case MetadataTransferException mte ->
+                                List.of(toFlatDependency(mte.getMetadata()));
+                            default -> List.of();
+                        }
+                ).flatMap(List::stream);
     }
 }
