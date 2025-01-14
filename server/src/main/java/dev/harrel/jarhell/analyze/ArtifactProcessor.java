8411940d3db6b73610f7f933bb27598e36b8d95f
Harrel <marcin.slowik95@gmail.com>
2025-01-14 19:31:39 +0100

    use simpler concurrency model?

---
+++ server/src/main/java/dev/harrel/jarhell/analyze/ArtifactProcessor.java
@@ -0,0 +1,86 @@
+package dev.harrel.jarhell.analyze;
+
+import dev.harrel.jarhell.model.Gav;
+import dev.harrel.jarhell.repo.ArtifactRepository;
+import dev.harrel.jarhell.util.ConcurrentUtil;
+import io.avaje.inject.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Closeable;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class ArtifactProcessor implements Closeable {
+    private static final Logger logger = LoggerFactory.getLogger(ArtifactProcessor.class);
+
+    private final ExecutorService service = Executors.newSingleThreadExecutor();
+    private final AtomicReference<Future<?>> runFuture = new AtomicReference<>(CompletableFuture.completedFuture(null));
+    private final AtomicBoolean running = new AtomicBoolean(false);
+    private final AtomicInteger concurrency = new AtomicInteger(1);
+    private final ArtifactRepository repo;
+    private final AnalyzeEngine analyzeEngine;
+
+    public ArtifactProcessor(ArtifactRepository repo, AnalyzeEngine analyzeEngine) {
+        this.repo = repo;
+        this.analyzeEngine = analyzeEngine;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        start(1);
+    }
+
+    public void start(int concurrency) {
+        if (running.get()) {
+            throw new IllegalStateException("Already running");
+        }
+        running.set(true);
+        this.concurrency.set(concurrency);
+        runFuture.set(service.submit(this::run));
+    }
+
+    public void stop() {
+        running.set(false);
+        try {
+            runFuture.get().get();
+        } catch (ExecutionException e) {
+            throw new CompletionException(e);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new CompletionException(e);
+        }
+    }
+    @Override
+    public void close() {
+        service.shutdown();
+        stop();
+    }
+
+
+    private void run() {
+        while (running.get()) {
+            Instant startTime = Instant.now();
+            try {
+                doRun();
+            } catch (Exception e) {
+                logger.warn("Batch failed", e);
+            }
+            logger.info("Batch finished in {}s", Duration.between(startTime, Instant.now()).toSeconds());
+        }
+    }
+
+    private void doRun() {
+        List<Gav> unresolvedGavs = repo.findAllUnresolved(concurrency.get(), 3);
+        logger.info("Fetched {} gavs for reanalysis", unresolvedGavs.size());
+        try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
+            unresolvedGavs.forEach(gav -> scope.fork(() -> analyzeEngine.doFullAnalysis(gav)));
+            ConcurrentUtil.joinScope(scope);
+        }
+    }
+}
--- server/src/main/java/dev/harrel/jarhell/controller/TechnicalController.java
+++ server/src/main/java/dev/harrel/jarhell/controller/TechnicalController.java
@@ -1,20 +1,24 @@
 package dev.harrel.jarhell.controller;
 
 import dev.harrel.jarhell.analyze.AnalyzeEngine;
+import dev.harrel.jarhell.analyze.ArtifactProcessor;
 import dev.harrel.jarhell.analyze.RepoWalker;
 import dev.harrel.jarhell.model.Gav;
 import io.avaje.config.Config;
 import io.avaje.http.api.Controller;
 import io.avaje.http.api.Post;
+import io.avaje.http.api.QueryParam;
 
 @Controller("/technical/v1")
 class TechnicalController {
     private final AnalyzeEngine analyzeEngine;
     private final RepoWalker repoWalker;
+    private final ArtifactProcessor artifactProcessor;
 
-    TechnicalController(AnalyzeEngine analyzeEngine, RepoWalker repoWalker) {
+    TechnicalController(AnalyzeEngine analyzeEngine, RepoWalker repoWalker, ArtifactProcessor artifactProcessor) {
         this.analyzeEngine = analyzeEngine;
         this.repoWalker = repoWalker;
+        this.artifactProcessor = artifactProcessor;
     }
 
     @Post("/crawl")
@@ -23,5 +27,15 @@ class TechnicalController {
                 Config.get("maven.repo-url"),
                 data -> analyzeEngine.saveUnresolved(new Gav(data.groupId(), data.artifactId(), data.versions().getLast()))
         ).join();
+    }
+
+    @Post("/processor/start")
+    void startProcessor(@QueryParam int concurrency) {
+        artifactProcessor.start(concurrency);
+    }
+
+    @Post("/processor/stop")
+    void stopProcessor() {
+        artifactProcessor.stop();
     }
 }
