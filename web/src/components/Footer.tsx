ce782f7f8eaa495b9e3b8b095faf61f44922700b
Harrel <marcin.slowik95@gmail.com>
2025-01-31 21:56:19 +0100

    carousel take

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -1,12 +1,41 @@
 import {PackageCard} from '@/components/PackageCard.tsx'
-import {ResolvedPackage} from '@/util.ts'
+import {gavToString, ResolvedPackage} from '@/util.ts'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious
+} from '@/shadcn/components/ui/Carousel.tsx'
 
 export const Footer = () => {
   const packages: ResolvedPackage[] = [
     {
       groupId: 'dev.harrel',
       artifactId: 'json-schema',
-      version: '1.7.3',
+      version: '1.7.0',
+      effectiveValues: {
+        size: 189_587,
+        bytecodeVersion: '52.0',
+        requiredDependencies: 0,
+        licenseType: 'MIT'
+      }
+    } as any,
+    {
+      groupId: 'dev.harrel',
+      artifactId: 'json-schema',
+      version: '1.7.1',
+      effectiveValues: {
+        size: 189_587,
+        bytecodeVersion: '52.0',
+        requiredDependencies: 0,
+        licenseType: 'MIT'
+      }
+    } as any,
+    {
+      groupId: 'dev.harrel',
+      artifactId: 'json-schema',
+      version: '1.7.2',
       effectiveValues: {
         size: 189_587,
         bytecodeVersion: '52.0',
@@ -28,7 +57,7 @@ export const Footer = () => {
     {
       groupId: 'dev.harrel',
       artifactId: 'json-schema',
-      version: '1.7.3',
+      version: '1.7.4',
       effectiveValues: {
         size: 189_587,
         bytecodeVersion: '52.0',
@@ -39,7 +68,18 @@ export const Footer = () => {
     {
       groupId: 'dev.harrel',
       artifactId: 'json-schema',
-      version: '1.7.3',
+      version: '1.7.5',
+      effectiveValues: {
+        size: 189_587,
+        bytecodeVersion: '52.0',
+        requiredDependencies: 0,
+        licenseType: 'MIT'
+      }
+    } as any,
+    {
+      groupId: 'dev.harrel',
+      artifactId: 'json-schema',
+      version: '1.7.6',
       effectiveValues: {
         size: 189_587,
         bytecodeVersion: '52.0',
@@ -51,13 +91,21 @@ export const Footer = () => {
   return (
     <div className='grow max-h-[800px] min-h-[800px] bg-gradient-to-b from-background to-lava-ambient p-12'>
       <div className='max-w-[1400px] m-auto'>
-        <div className='flex gap-8'>
-          <h2
-            className='[writing-mode:sideways-lr] text-center text-2xl font-semibold leading-none tracking-tight text-fade'>Recently
-            analyzed</h2>
-          {packages.map(pkg =>
-            <PackageCard pkg={pkg}/>
-          )}
+        <div className='flex gap-16'>
+          <h2 className='[writing-mode:sideways-lr] text-center text-2xl font-semibold leading-none tracking-tight text-fade'>
+            Recently analyzed
+          </h2>
+          <Carousel opts={({dragFree: true})} className='min-w-0'>
+            <CarouselContent className='py-2'>
+              {packages.map(pkg =>
+                <CarouselItem key={gavToString(pkg)} className='basis-1/4'>
+                  <PackageCard pkg={pkg}/>
+                </CarouselItem>
+              )}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
         </div>
       </div>
     </div>
