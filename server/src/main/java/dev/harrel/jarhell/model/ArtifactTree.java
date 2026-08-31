2102eb2119b2b3299b9a0d0ab40f4a6a96cc366d
harrel <marcin.slowik95@gmail.com>
2026-08-31 19:27:55 +0200

    unused imp

--- server/src/main/java/dev/harrel/jarhell/model/ArtifactTree.java
+++ server/src/main/java/dev/harrel/jarhell/model/ArtifactTree.java
@@ -1,7 +1,5 @@
 package dev.harrel.jarhell.model;
 
-import com.fasterxml.jackson.annotation.JsonUnwrapped;
-
 import java.util.List;
 
 public record ArtifactTree(ArtifactInfo artifactInfo, List<DependencyInfo> dependencies) {}
