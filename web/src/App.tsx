1cbe89c9b65fa705d2ae1c87354958f5e945cad1
harrel <marcin.slowik95@gmail.com>
2026-09-03 22:06:38 +0200

    start impl with topbar

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,29 +1,26 @@
-import { Title } from '@solidjs/meta';
-import { Loading } from 'solid-js';
-import './App.css';
-import {createRouter} from '@solidjs/router'
+import { Loading } from 'solid-js'
+import { createRouter } from '@solidjs/router'
+import TopBar from './components/TopBar'
+import './App.css'
 
 const Router = createRouter({
   routes: [
-    {
-      path: '/index', component: () => <p>index</p>
-    },
-    {
-      path: '*404', component:() =>  <p>not found</p>
-    }
-  ]
+    { path: '/', component: () => <p>index</p> },
+    { path: '*404', component: () => <p>not found</p> },
+  ],
 })
 
 export default function App() {
   return (
     <Router>
-      {(props) => (
+      {props => (
         <>
+          <TopBar/>
           <Loading fallback={<main class="px-4 py-12">Loading…</main>}>
             {props.children}
           </Loading>
         </>
       )}
     </Router>
-  );
+  )
 }
