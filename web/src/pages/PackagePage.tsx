cd58dabe292de5abb2299f0450c35a1f0c08e8fa
harrel <marcin.slowik95@gmail.com>
2026-09-13 23:57:12 +0200

    navigation hell finally fixed?

--- web/src/pages/PackagePage.tsx
+++ web/src/pages/PackagePage.tsx
@@ -1,11 +1,11 @@
-import {createMemo, Errored, isPending, latest, Loading, Show, untrack} from 'solid-js'
-import { useParams } from '@solidjs/router'
+import {createEffect, createMemo, createSignal, Errored, latest, Loading, Show} from 'solid-js'
+import {useParams} from '@solidjs/router'
 import { VersionsSidebar, VersionsSidebarSkeleton } from '../components/VersionsSidebar'
 import { NotFoundView, ThrownErrorView } from '../components/ErrorView'
 import { PackageView } from '../components/package/PackageView'
 import { PackageMainSkeleton } from '../components/package/PackageMainSkeleton'
 import {analyzePackage, ArtifactTree, getPackage, getVersions} from '../api'
-import {formatGav, Gav, parseGav} from '../utils/gav'
+import {formatGav, parseGav} from '../utils/gav'
 
 const isNotFound = (tree: ArtifactTree) =>
   tree.artifactInfo.unresolved === true && (tree.artifactInfo.unresolvedReason?.includes('ArtifactNotFoundException') ?? false)
@@ -15,8 +15,22 @@ export function PackagePage() {
   const gav = createMemo(() => parseGav(params['coordinate'])!)
 
   const versions = createMemo(() => getVersions(gav().groupId, gav().artifactId, gav().classifier))
-  const isAnalyzed = (gav: Gav) => untrack(versions).some(av => av.version === gav.version && av.analyzed)
-  const pkg = createMemo(() => isAnalyzed(gav()) ? getPackage(formatGav(gav())) : analyzePackage(gav()))
+  const analyzed = createMemo(() => versions().some(av => av.version === gav().version && av.analyzed))
+  const [pkg, setPkg] = createSignal(() => (gav(), analyzed()) ? getPackage(formatGav(gav())) : undefined)
+
+  // don't wait reactively for analysis to finish, but we want to get notified when it finishes (and use it returnde data)
+  createEffect(
+    () => analyzed() ? undefined : gav(),
+    g => {
+      if (g) {
+        analyzePackage(g).then(data => {
+          if (g === latest(gav)) {
+            setPkg(data)
+          }
+        })
+      }
+    }
+  )
 
   return (
     <div class="mx-auto flex w-full max-w-(--measure-app) items-start">
@@ -25,12 +39,15 @@ export function PackagePage() {
       </Loading>
 
       <Errored fallback={err => <ThrownErrorView error={err()}/>}>
-        <Loading fallback={<PackageMainSkeleton gav={latest(gav)} analyzing={!isPending(versions) && !isAnalyzed(latest(gav))}/>}>
-          <Show when={!isPending(pkg) || isAnalyzed(latest(gav))} fallback={<PackageMainSkeleton gav={latest(gav)} analyzing={!isAnalyzed(latest(gav))}/>}>
-            <Show when={!isNotFound(pkg())} fallback={<NotFoundView/>}>
-              <PackageView tree={pkg()}/>
+        <Loading on={analyzed} fallback={<PackageMainSkeleton gav={gav()} analyzing={false}/>}>
+          <Loading fallback={<PackageMainSkeleton gav={gav()} analyzing={!analyzed()}/>}>
+            <Show when={pkg()} fallback={<PackageMainSkeleton gav={gav()} analyzing={!analyzed()}/>}>
+              {pkg =>
+                <Show when={!isNotFound(pkg())} fallback={<NotFoundView/>}>
+                  <PackageView tree={pkg()}/>
+                </Show>}
             </Show>
-          </Show>
+          </Loading>
         </Loading>
       </Errored>
     </div>
